47c4107aeaa065957d65155f03a2395fc472df88
denissb <spawnenius@gmail.com>
2020-03-17 09:55:57 +0100

    fix: alignment

--- src/store/Native.ts
+++ src/store/Native.ts
@@ -1,20 +1,18 @@
-/* tslint:disable */
-const AsyncStorage = require('@react-native-community/async-storage');
-/* tslint:enable */
+import AsyncStorage from '@react-native-community/async-storage';
 
 import IStore from '../IStore';
 
 class NativeStore implements IStore {
   public async getItem(key) {
-    return AsyncStorage.getItem ? AsyncStorage.getItem(key) : AsyncStorage.default.getItem(key);
+    return AsyncStorage.getItem(key);
   }
 
   public async setItem(key, value) {
-    AsyncStorage.setItem ? AsyncStorage.setItem(key, value) : AsyncStorage.default.setItem(key, value);
+    AsyncStorage.setItem(key, value);
   }
 
   public async removeItem(key) {
-    AsyncStorage.removeItem ? AsyncStorage.removeItem(key) : AsyncStorage.default.removeItem(key);
+    AsyncStorage.removeItem(key);
   }
 }
 
