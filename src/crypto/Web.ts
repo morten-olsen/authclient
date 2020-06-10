b8ce5e0b04c9ae39bf1ad1579ef32adadd7abbd7
Morten Olsen <morten@olsen.pro>
2020-06-10 06:55:12 +0200

    Align with NPM

--- src/crypto/Web.ts
+++ src/crypto/Web.ts
@@ -7,6 +7,7 @@ const base64 = require('base-64');
 
 const CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const HAS_CRYPTO = typeof window !== 'undefined' && !!(global as any).crypto;
+const encoder = (global as any).TextEncoder ? new (global as any).TextEncoder('utf-8') : undefined;
 
 export const bufferToString = (buffer) => {
   const state = [];
@@ -19,13 +20,35 @@ export const bufferToString = (buffer) => {
   return state.join('').replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
 };
 
+const arrayBufferToBase64 = (buffer) => {
+     let binary = '';
+     const bytes = new global.Uint8Array(buffer);
+     const len = bytes.byteLength;
+     for (let i = 0; i < len; i += 1) {
+         binary += String.fromCharCode(bytes[i]);
+     }
+     return base64.encode(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+ };                                                                                                                
+ const hexToBytes = (input) => {  
+  var bytes = new Uint8Array(Math.ceil(input.length / 2));                                                    for (var i = 0; i < bytes.length; i++)                                                          
+    bytes[i] = parseInt(input.substr(i * 2, 2), 16);      
+   return bytes;                                                                                       
+ }; 
+
 class NodeCrypto implements ICrypto {
   public async sha256(input: string) {
-    return sha(input);
+    if (HAS_CRYPTO && encoder) {                                                                          
+      const encoded = encoder.encode(input);                                                            
+      const hashed = await (global as any).crypto.subtle.digest('SHA-256', encoded);                             
+      return hashed;                                                                                    
+    } else {                                                                                                
+      const hashed = sha(input);                                                                        
+      return hexToBytes(hashed);                                                                        
+    }
   }
 
   public async base64UrlEncode(value: string) {
-    return base64.encode(value).replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '');
+    return arrayBufferToBase64(value);
   }
 
   public async random(type: string) {
